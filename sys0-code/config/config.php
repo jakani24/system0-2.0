f21470776cd69fc4026d8fcfc029f6ab2f40fec7
Janis Steiner <janis.st44@gmail.com>
2024-06-17 20:27:07 +0100

    removing secrets

--- sys0-code/config/config.php
+++ sys0-code/config/config.php
@@ -1,15 +1,14 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '1234');
-define('DB_NAME', 'system0');
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
 $api=""; //the telegram api key for jakach notification system
 $SENDGRID_API_KEY=""; //our new api key, for the new mail address
 $sendgrid_email=""; //our new email
 $chat_id=""; //chat id of the admin => janis steiner
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
