d059a483797faa70adef563ea937ca8fea8e7060
Janis Steiner <janis.st44@gmail.com>
2024-10-14 15:00:02 +0100

    fixing bug where filamanet usage was set for wrong user

--- sys0-code/app/overview.php
+++ sys0-code/app/overview.php
@@ -121,6 +121,7 @@ function load_user()
                                 <?php
                                         if(isset($_GET['free'])&&$_GET["rid"]==($_SESSION["rid"]-1))
                                         {
+						$cnt="";
                                                 $printer_id=htmlspecialchars($_GET['free']);
                                                 $sql="select used_by_userid from printer where id=$printer_id";
                                                 $stmt = mysqli_prepare($link, $sql);
@@ -150,7 +151,7 @@ function load_user()
 						$userid=$_SESSION["id"];
 						if(isset($json['job']['filament']['tool0']['length'])){
 							$filament_usage=intval($json['job']['filament']['tool0']['length']);
-							$sql="UPDATE users SET filament_usage = COALESCE(filament_usage,0) + $filament_usage WHERE id = $userid";
+							$sql="UPDATE users SET filament_usage = COALESCE(filament_usage,0) + $filament_usage WHERE id = $cnt";
 							//echo($sql);
 							$stmt = mysqli_prepare($link, $sql);
                                                 	mysqli_stmt_execute($stmt);
