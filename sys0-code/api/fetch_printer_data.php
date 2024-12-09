9e44d8d95b3379f9c332948f7dfa8725763e2978
Janis Steiner <janis.st44@gmail.com>
2024-12-09 15:43:08 +0000

    moving some api files to /api/

--- sys0-code/api/fetch_printer_data.php
+++ sys0-code/api/fetch_printer_data.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: /login/login.php");
+    exit;
+}
+
 include "../config/config.php";
 
 header('Content-Type: application/json');
