8c614b14b507004169b06b7ad86e1f1e973335e1
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2024-12-16 20:27:49 +0000

    Update components.php

--- sys0-code/assets/components.php
+++ sys0-code/assets/components.php
@@ -5,6 +5,6 @@ echo('<meta charset="utf-8">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
-<script src="https://kit.fontawesome.com/8ab5a73cf2.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/c4deda69dc.js" crossorigin="anonymous"></script>
 ');
 ?>
