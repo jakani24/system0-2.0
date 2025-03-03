7cedb337839ad01948b8b8824e6669798a6fb88d
Janis Steiner <janis@jakach.ch>
2025-03-03 16:22:51 +0000

    fixing layout bug

--- sys0-code/app/print.php
+++ sys0-code/app/print.php
@@ -13,7 +13,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>System0 - Print</title>
-		<?php include "../../assets/components.php"; ?>
+		<?php include "../assets/components.php"; ?>
 	</head>
 	<body>
 		<script src="/assets/js/load_page.js"></script>
